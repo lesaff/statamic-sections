c83f0b1739dc9228c34d4d478335f08e5c20ab30
Rudy Affandi <rudy@adnetinc.com>
2016-08-05 17:10:09 -0300

    Fixed folder mess

--- Sections/SectionsListener.php
+++ Sections/SectionsListener.php
@@ -21,8 +21,14 @@ class SectionsListener extends Listener
      */
     public function sectionsCSS()
     {
+        // Get UI type from addon settings
+        $ui_type = $this->getConfig('ui_type');
+
         $addon_css = $this->css->url('sections.css');
-        $html = '<link rel="stylesheet" href="' . $addon_css . '">';
+        $html  = '<link rel="stylesheet" href="' . $addon_css . '" />';
+        $html .= '<script type="text/javascript">';
+        $html .= 'var SectionsUI = \'' . $ui_type . '\';';
+        $html .= '</script>';
         return $html;
     } 
 }
